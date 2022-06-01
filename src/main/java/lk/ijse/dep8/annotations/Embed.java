c8feca5d472430570e2b5fa0f1e0c4eb8d0538a2
Thakshila Nuwangi <thakshilanuwangi123@gmail.com>
2022-06-01 08:03:04 +0530

    add required annotations and their properties

---
+++ src/main/java/lk/ijse/dep8/annotations/Embed.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright © 2022 - present Thakshila Nuwangi. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+ */
+
+package lk.ijse.dep8.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Embed {
+}
